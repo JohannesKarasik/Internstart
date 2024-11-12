1c7483f5a5487a1432471894be40d5c8531cb29a
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2024-11-12 12:34:00 -0500

    Add .gitignore file and update project files

--- studybud/urls.py
+++ studybud/urls.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
 
-
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
-    path('api/', include('base.api.urls'))
+    path('api/', include('base.api.urls')),
+    path("health/", health_check, name="health_check"),
+
 ]
 
 if settings.DEBUG:
