2117fc13f71780d99206ade3e3a00279ebe79c67
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2025-09-30 21:59:33 +0200

    Initial commit

--- studybud/settings.py
+++ studybud/settings.py
@@ -70,14 +70,12 @@ if DEBUG:
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Internstart <contact@internstart.com>")
-
-# templates
+EMAIL_HOST_USER = "contact@internstart.com"
+EMAIL_HOST_PASSWORD = "xvtacurmxcibcugp"  # your 16-char App Password
+DEFAULT_FROM_EMAIL = "Internstart <contact@internstart.com>"
 
 CSRF_TRUSTED_ORIGINS = [
     "http://167.99.242.34",   # your server IP
