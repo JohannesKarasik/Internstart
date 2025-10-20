2cb8f98ea1c523f9d0cf756392e83b487e2cd781
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2025-10-21 00:00:22 +0200

    Initial commit

--- base/context_processors.py
+++ base/context_processors.py
@@ -1,5 +1,7 @@
 from .models import Connection  # Make sure to import the Connection model
 from django.db.models import Q
+import os
+from django.conf import settings
 
 def global_connections(request):
     # If the user is not authenticated, return an empty context
@@ -9,6 +11,14 @@ def global_connections(request):
     # Otherwise, return the user's connections
     connections = Connection.objects.filter(Q(user=request.user) | Q(connection=request.user))
     return {'connections': connections}
+
+
+
+def stripe_public_key(request):
+    return {
+        "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY
+    }
+
 
 
 
