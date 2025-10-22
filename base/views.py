8e68f35759dad6d20c349c866fafe93c77c1d7a3
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2025-10-22 15:28:42 +0200

    Initial commit

--- base/views.py
+++ base/views.py
@@ -115,6 +115,9 @@ from .models import SavedJob
 
 import re
 from django.core.paginator import Paginator
+from django.contrib.admin.views.decorators import staff_member_required
+from .openai_utils import extract_job_data
+
 
 
 
